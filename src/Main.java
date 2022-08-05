3c3bcf9a76d8fb9c9c1d977c5308f0458e062e82
dkoh05 <dykoh05@gmail.com>
2022-08-05 19:50:47 +0700

    changed database path

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-//		LoginGUI loginPage = new LoginGUI(); // open the program
-		ReservationGUI reservPage = new ReservationGUI(null);
+		LoginGUI loginPage = new LoginGUI(); // open the program
+//		ReservationGUI reservPage = new ReservationGUI(null);
 	}
 
 }
