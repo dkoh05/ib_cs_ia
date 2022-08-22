514a36e1ad0fe13298c7163ca060d4bfdedba467
dkoh05 <dykoh05@gmail.com>
2022-08-23 01:05:37 +0900

    implemented further UI for pendingOrder admin page

--- src/PendingOrderGUI.java
+++ src/PendingOrderGUI.java
@@ -6,11 +6,21 @@ import java.awt.event.ActionListener;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.border.Border;
+
+import com.github.lgooddatepicker.components.DatePicker;
+import com.github.lgooddatepicker.components.TimePicker;
 
 import java.sql.Connection;
 
@@ -26,9 +36,34 @@ public class PendingOrderGUI implements ActionListener{
 	private static JButton usersListBtn = new JButton("USERS LIST");
 	private static JButton logoutBtn = new JButton("LOGOUT");
 	
+
+	JLabel bookingLabel = new JLabel("Booking Reservation");
+	JLabel guestNameLabel = new JLabel("Name: ");
+	JTextField guestNameText = new JTextField();
+	JLabel guestNumLabel = new JLabel("Number of Guests: ");
+	SpinnerModel guestNumModel = new SpinnerNumberModel(1, 1, 10, 1);    
+	JSpinner guestNumSpinner = new JSpinner(guestNumModel);
+	
+	JLabel checkinDateLabel = new JLabel("Check-in Date: ");
+	DatePicker checkinDatePicker = new DatePicker();
+	JLabel checkinTimeLabel = new JLabel("Check-in Time: ");
+	TimePicker checkinTimePicker = new TimePicker();
+	
+	JLabel checkoutDateLabel = new JLabel("Check-out Date: ");
+	DatePicker checkoutDatePicker = new DatePicker();
+	JLabel checkoutTimeLabel = new JLabel("Check-out Time: ");
+	TimePicker checkoutTimePicker = new TimePicker();
+	
+	JLabel noteLabel = new JLabel("Special Requests: ");
+	JTextField noteText = new JTextField();
+	
+	JButton saveBtn = new JButton("SAVE");
+	JButton cancelBtn = new JButton("CANCEL ORDER"); // cencel order
+	JButton completedBtn = new JButton("COMPLETE ORDER"); // click when an order is completed
+	
 	
 	PendingOrderGUI(){
-		frame.setSize(1200, 900);
+		frame.setSize(1200, 800);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		panel.setLayout(null);
@@ -54,9 +89,57 @@ public class PendingOrderGUI implements ActionListener{
 		logoutBtn.addActionListener(this);
 		panel.add(logoutBtn);
 		
+		guestNameLabel.setBounds(100, 550, 150, 25);
+		guestNameText.setBounds(150, 550, 150, 25 );
+		panel.add(guestNameLabel);
+		panel.add(guestNameText);
+		
+		guestNumLabel.setBounds(100, 600, 150, 25);
+		
+		panel.add(guestNumLabel);
+
+		guestNumSpinner.setBounds(250, 600, 50 , 25);
+		Border line = BorderFactory.createLineBorder(Color.black, 1);
+		guestNumSpinner.setBorder(line);
+		panel.add(guestNumSpinner);
+		
+		checkinDateLabel.setBounds(350, 550, 150, 25);
+		checkinDatePicker.setBounds(450, 550, 165, 25);
+		panel.add(checkinDateLabel);
+		panel.add(checkinDatePicker);
+		
+		checkinTimeLabel.setBounds(350, 600, 150, 25);
+		checkinTimePicker.setBounds(450, 600, 150, 25);
+		panel.add(checkinTimeLabel);
+		panel.add(checkinTimePicker);
+		
+		checkoutDateLabel.setBounds(700, 550, 150, 25);
+		checkoutDatePicker.setBounds(800, 550, 165, 25);
+		panel.add(checkoutDateLabel);
+		panel.add(checkoutDatePicker);
+		
+		checkoutTimeLabel.setBounds(700, 600, 150, 25);
+		checkoutTimePicker.setBounds(800, 600, 150, 25);
+		panel.add(checkoutTimeLabel);
+		panel.add(checkoutTimePicker);
+		
+		noteLabel.setBounds(100, 650, 150, 25);
+		noteText.setBounds(225, 650, 750, 25);
+		panel.add(noteLabel);
+		panel.add(noteText);
+		
+		saveBtn.setBounds(100, 700, 150, 25);
+		cancelBtn.setBounds(300, 700, 150, 25);
+		completedBtn.setBounds(500, 700, 150, 25);
+		panel.add(saveBtn);
+		panel.add(cancelBtn);
+		panel.add(completedBtn);
+		
+		
+		
 		// change the data and column names to correspond with database
 
-        
+
 		Connection con = SQLConnect.connect();
 		
 		String query = "SELECT * from reservation where is_completed = 0;";
